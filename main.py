bd17ea6b7df82a2cbbf56541e5cc98e3f6bb27c0
KevinLprojects <leroy.kevinjames@gmail.com>
2025-10-04 21:04:28 -0400

    multiline statement support (NO MULTILINE STRINGS)

--- main.py
+++ main.py
@@ -2,18 +2,20 @@
 General notes:
 
 I'm not going to include any control flow that happens within a single line (ternarys). 
-I'm also not going to include nested function declarations, because this problem is already too hard.
+I'm also not going to include nested function declarations, because this problem is already too hard. 
 I'm also not going to include yield, because I might kill myself.
 
 The flowchart will have one start and end node that represent all start and end conditions
 The flowchart will have separate graphs to represent each function. 
 
-A function will have a terminal node representing the start (with inputs) and an end terminal node with outputs. 
+A function will have a terminal node representing the start (with inputs) and an end terminal node with outputs.
 If no return, the terminal name will say None, just like how the python interpreter interprets it.
 
-Classes will have very basic graphs showing attributes and lists of methods (methods will have flowcharts same as functions)
+Classes will have very basic graphs showing attributes and lists of methods (methods will have flowcharts same as functions) 
 Subclasses will have arrows pointing to the parent class
 """
+
+# FOR NOW MULTILINE STRINGS with ''' or """ don't work
 
 import sys
 import graphviz
@@ -42,8 +44,8 @@ Keyword_Map = {
     "except": ["diamond", generic],
     "finally": ["diamond", generic],
     "with": ["diamond", generic],
-    "def": ["diamond", generic],
-    "class": ["diamond", generic]
+    "def": ["box", generic],
+    "class": ["box", generic]
 }
 
 class Block:
@@ -85,7 +87,7 @@ class Block:
         for child in self.children:
             child.graph()
 
-def num_indentation(line):
+def num_indentation(line, i):
     # check for tab character
     if line[0] == '\t':
         count = 0
@@ -102,7 +104,7 @@ def num_indentation(line):
         num_intdent = (len(line) - len(line.strip())) / 4
         # if the user is a 
         if not num_intdent.is_integer():
-            raise IndentationError(f"Bro. {num_intdent * 4} spaces? What is wrong with you?")
+            raise IndentationError(f"Bro. {num_intdent * 4} spaces at line {i}? What is wrong with you?")
         
         return int((len(line) - len(line.strip())) / 4)
     
@@ -113,27 +115,53 @@ def remove_lines(lines):
     # iterate through the lines in reverse order, so that lines can be removed
     for i in reversed(range(len(lines))):
         # remove new line characters
-        if lines[i].endswith('\n'):
-            lines[i] = lines[i][0:-1]
-
+        lines[i] = lines[i].rstrip()
         line = lines[i]
 
         # if multiline comment ends, set open_multiline to false, and remove the line
-        if ("\"\"\"" in line or "\'\'\'" in line) and open_multiline == True:
+        if (line.strip().startswith("\"\"\"") or line.strip().startswith("\'\'\'")) and open_multiline == True:
             open_multiline = False
             lines.pop(i)
+            print("comment ended")
             continue
 
         # if multiline comment starts, then set open_multiline to True
-        elif ("\"\"\"" in line or "\'\'\'" in line) and open_multiline == False:
+        elif (line.strip().startswith("\"\"\"") or line.strip().startswith("\'\'\'")) and open_multiline == False:
             open_multiline = True
+            print("comment start")
         
         # remove comments and empty lines
-        if open_multiline or len(line) == 0 or line.isspace() or line.strip()[0] == '#':
+        if open_multiline or len(line) == 0 or line.strip()[0] == '#':
             lines.pop(i)
+            print("in comment")
             continue
+
+        print('\n')
         
-        lines[i] = [num_indentation(line), line.strip()]
+        lines[i] = [num_indentation(line, i + 1), line.strip()]
+
+def merge_lines(lines):
+    open_parentheses = 0
+    open_square = 0
+    open_curly = 0
+    last_state = False
+    combined_lines = ""
+
+    for i in reversed(range(len(lines))):
+        open_parentheses += lines[i][1].count(")") - lines[i][1].count("(")
+        open_square += lines[i][1].count("]") - lines[i][1].count("[")
+        open_curly += lines[i][1].count("}") - lines[i][1].count("{")
+
+        if open_parentheses + open_square + open_curly > 0:
+            combined_lines = lines[i][1] + '\n' + combined_lines
+            lines.pop(i)
+            last_state = True
+        else:
+            if last_state == True:
+                lines[i][1] += '\n' + combined_lines
+                combined_lines = ""
+            last_state = False
+
 
 def main(file_name = __file__):
     # open the except if the file can't be opened
@@ -147,6 +175,12 @@ def main(file_name = __file__):
 
     # process the text
     remove_lines(lines)
+    merge_lines(lines)
+
+    # for line in lines:
+    #     print(line)
+
+    # exit()
 
     dot = graphviz.Digraph()
     blocks = []
